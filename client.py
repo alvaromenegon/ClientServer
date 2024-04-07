1104d9d491b08bf4e4e851b5121da63679ac2af4
Alvaro Eduardo Menegon Rosario <104403541+alvaromenegon@users.noreply.github.com>
2024-04-07 11:40:17 +0100

    permite alterar host e port

--- client.py
+++ client.py
@@ -4,10 +4,14 @@ import socket
 #tchau encerra
 
 def client_program():
-    host = socket.gethostname() 
-    port = 5000 
+    host = input('Digite o IP do servidor, ou pressione enter para usar o localhost: ') or '127.0.0.1'
+    port = int(input('Digite a porta do servidor, ou pressione enter para usar a porta padrão 5000: ') or 5000)
+    #host = socket.gethostname() 
+    #port = 5000 
+    print('Conectando ao servidor em ' + host + ' na porta ' + str(port))
     client_socket = socket.socket() 
     client_socket.connect((host, port)) 
+    print('Conectado ao servidor ' + host + ' na porta ' + str(port))
     serverName = client_socket.recv(1024).decode()
     print('Conectado ao Servidor ' + serverName)
     client_socket.send(clientName.encode()) 
